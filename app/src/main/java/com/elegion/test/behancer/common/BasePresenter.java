3a4df1524c413f7055fbf0132c965d7fc0917ebc
alien1900 <alien1900@bk.ru>
2018-05-30 14:04:03 +0700

    Initial commit

---
+++ app/src/main/java/com/elegion/test/behancer/common/BasePresenter.java
@@ -0,0 +1,13 @@
+package com.elegion.test.behancer.common;
+
+import io.reactivex.disposables.CompositeDisposable;
+
+public abstract class BasePresenter {
+
+    protected CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+
+    public void disposeAll() {
+        mCompositeDisposable.clear();
+    }
+
+}
